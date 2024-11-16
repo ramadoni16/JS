a841d88324e57a24752ed01a2e278c9b227f5cf4
ramadoni16 <donicyberit@gmail.com>
2024-11-16 23:44:06 +0700

    Tips Memberikan Nama Variabel

---
+++ app.js
@@ -0,0 +1,13 @@
+// variabel let
+
+// let tahunMredeka = 1945;
+// let lamaTahunMerdeka = 2022 - tahunMredeka;
+// console.log(lamaTahunMerdeka);
+
+// variabel const
+// const nilaiV = 3.14;
+
+let namaLengkap = "Camel Case";
+let NamaLengkap = "Pascal Case";
+let nama_lengkap = " Snake Case";
+let $variabel = "simbol";
